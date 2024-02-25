013752c890a6167a85e42f3e7e53e89cee852961
chryzle <104879763+chryzanths@users.noreply.github.com>
2024-02-25 14:59:35 +0800

    Delete GameApp.java

--- GameApp.java
+++
@@ -1,8 +0,0 @@
-public class GameApp{
-    public static void main(String[] args){
-        Character character = new Character("Knight", new Knight());
-
-        System.out.println(character.attack());
-        System.out.println(character.defend());
-    }
-}
